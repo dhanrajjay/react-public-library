c6df42ac4162175b7c110dd441314a0c4273756a
Dhanraj V <dhanrajv@dhanrajv-a01.vmware.com>
2020-06-23 15:06:48 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/app.css';
+
+class App extends Component {
+	render() {
+		return(
+			<div className="app">Test</div>
+		)
+	}
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+
+module.exports = (env, args) => {
+	const config = {
+		// used to know which file caused the error, in the minified compressed files.
+		devtool: (args.mode === 'development') ? 'inline-source-map' : false,
+		// To place the output in build directory
+		output: {
+			filename: "bundle.js",
+			path: __dirname + "/dist"
+		},
+		module: {
+			rules: [
+			  // js & jsx minification
+			  {
+				test: /\.(js|jsx)$/,
+			    exclude: /node_modules/,
+			    use: {
+			    	loader: "babel-loader"
+			    }
+			  },
+			  // html minification
+			  {
+			    test: /\.html$/,
+			    use: {
+			    	loader: "html-loader"
+			    }
+			  },
+			  // css minification
+			  {
+			  	test: /\.(s*)css$/,
+			  	use: [
+		          'sass-loader'
+		        ],
+			  }
+			]
+		},
+		optimization: {
+			splitChunks: {
+				cacheGroups: {
+					commons: {
+						test: /[\\/]node_modules[\\/]/,
+						name: 'vendors',
+						chunks: 'all'
+					}
+				}
+			}
+		},
+		plugins: [
+			new HtmlWebPackPlugin({
+				template: "./src/index.html",
+				filename: "./index.html"
+			})
+			new MinifyPlugin()
+		]
+	}
+
+	return config;
+};
