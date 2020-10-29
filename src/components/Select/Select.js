6dea834d4c85960c69e597bc0a83d21d3295dab5
dhanraj.jay <dhanraj.jay@gmail.com>
2020-10-29 10:24:34 +0530

    select updated

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,63 +1,73 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, forwardRef, useRef, useImperativeHandle } from "react";
 import { Form, FormControl } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import "./Select.css";
 
-const Select = ({
-  name,
-  label,
-  list,
-  onChange,
-  className,
-  value,
-  url,
-  errorCB,
-  ...props
-}) => {
-    const listOption = list && list.map(item=>
-       <option key={item.value} value={item.value}> {item.label} </option>
-    );
-    const [loading, setLoading] = useState(true);
-    const [optionItems, setOptionItems] = useState(listOption);
+const Select = forwardRef((props, ref) => {
+  console.log(props)
+  const { name,
+            label,
+            list,
+            onChange,
+            className,
+            value,
+            url,
+            errorCB} = {...props}
+  const listOption = list && list.map(item=>
+     <option key={item.value} value={item.value}> {item.label} </option>
+  );
+  const [loading, setLoading] = useState(true);
+  const [optionItems, setOptionItems] = useState(listOption);
 
-    const handleChange = (event) => {
-        onChange(event);
+  const handleChange = (event) => {
+      onChange(event);
+  }
+
+  useEffect(() => {
+      if (list && list.length) {
+          setLoading(false);
+          return;
+      }
+      const apiUrl = url;
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((list) => {
+          setOptionItems(list.results.map(item=>
+             <option key={item.name} value={item.name}> {item.name} </option>
+          ));
+          setLoading(false);
+        }, (error) => {
+          setLoading(false);
+          errorCB('Failed to load the list');
+        });
+   }, [setLoading]);
+
+  useImperativeHandle(ref, () => ({
+
+    dynamicSelectCb(data) {
+        console.log('dyanic data updated')
+        const listOption = data && data.map(item=>
+           <option key={item.value} value={item.value}> {item.label} </option>
+        );
+        setOptionItems(listOption);
     }
 
-    useEffect(() => {
-        if (list && list.length) {
-            setLoading(false);
-            return;
-        }
-        const apiUrl = url;
-        fetch(apiUrl)
-          .then((res) => res.json())
-          .then((list) => {
-            setOptionItems(list.results.map(item=>
-               <option key={item.name} value={item.name}> {item.name} </option>
-            ));
-            setLoading(false);
-          }, (error) => {
-            setLoading(false);
-            errorCB('Failed to load the list');
-          });
-     }, [setLoading]);
+  }));
 
   return (
-    <div>
-    { loading ? <div className="loader"></div> :
-        (<Form.Group>
-                <Form.Label htmlFor={name}>{label}</Form.Label>
-                <Form.Control value={value} className={className}
-                              name={name} as="select"
-                              onChange={handleChange}>
-                    {optionItems}
-                </Form.Control>
-        </Form.Group>)
-    }
-    </div>
+      <div>
+      { loading ? <div className="loader"></div> :
+          (<Form.Group>
+                  <Form.Label htmlFor={name}>{label}</Form.Label>
+                  <Form.Control value={value} className={className}
+                                name={name} as="select"
+                                onChange={handleChange}>
+                      {optionItems}
+                  </Form.Control>
+          </Form.Group>)
+      }
+      </div>
   )
-};
+});
 
 Select.defaultProps = {
     className: "simple-select-button",
